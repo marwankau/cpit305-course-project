98ce112567860aff35eca553af9da9a06d7f273a
Basem <basem.bahattab.bm@gmail.com>
2022-10-25 03:43:12 +0300

    update GameGUI & Game

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,17 +1,99 @@
 package Game;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import GUI.GameGUI;
+
 public class Game {
 
-    String lobbyName;
-    int turn = 0; // 0 -> X || 1 -> O
-
+    private String lobbyName;
+    private static boolean sign = true; // TRUE -> X || FALSE -> O
+    public static int counter = 0;
+    private static char[][] gameboard = new char[3][3];
+    private ImageIcon XIcon = new ImageIcon("Game picture/X.png");
+    private ImageIcon OIcon = new ImageIcon("Game picture/O.png");
     // public Game(String lobbyName) {
-    //     this.lobbyName = lobbyName;
+    // this.lobbyName = lobbyName;
     // }
-    
-    public Game(){
 
+    public Game() {}
+
+    public JLabel setsignInGUI(GameGUI gui, JLabel label, int x, int y) {
+        
+
+
+        if (sign == true) {
+            label.setIcon(XIcon);
+            gameboard[x][y] = 'X';
+
+            sign = !sign;
+            counter++;
+        } else {
+            label.setIcon(OIcon);
+            gameboard[x][y] = 'O';
+            
+            sign = !sign;
+            counter++;
+        }
+        return label;
     }
-    
-    
+
+    public boolean winner() {
+        // Horizontal Win
+        if (gameboard[0][0] == 'X' && gameboard[0][1] == 'X' && gameboard[0][2] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[0][0] == 'O' && gameboard[0][1] == 'O' && gameboard[0][2] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } else if (gameboard[1][0] == 'X' && gameboard[1][1] == 'X' && gameboard[1][2] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[1][0] == 'O' && gameboard[1][1] == 'O' && gameboard[1][2] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } else if (gameboard[2][0] == 'X' && gameboard[2][1] == 'X' && gameboard[2][2] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[2][0] == 'O' && gameboard[2][1] == 'O' && gameboard[2][2] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } // Vertical Wins
+        else if (gameboard[0][0] == 'X' && gameboard[1][0] == 'X' && gameboard[2][0] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[0][0] == 'O' && gameboard[1][0] == 'O' && gameboard[2][0] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } else if (gameboard[0][1] == 'X' && gameboard[1][1] == 'X' && gameboard[2][1] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[0][1] == 'O' && gameboard[1][1] == 'O' && gameboard[2][1] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } else if (gameboard[0][2] == 'X' && gameboard[1][2] == 'X' && gameboard[2][2] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[0][2] == 'O' && gameboard[1][2] == 'O' && gameboard[2][2] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } // Diagonal Wins
+        else if (gameboard[0][0] == 'X' && gameboard[1][1] == 'X' && gameboard[2][2] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[0][0] == 'O' && gameboard[1][1] == 'O' && gameboard[2][2] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        } else if (gameboard[0][2] == 'X' && gameboard[1][1] == 'X' && gameboard[2][0] == 'X') {
+            System.out.println("Player 1 Wins");
+            return true;
+        } else if (gameboard[0][2] == 'O' && gameboard[1][1] == 'O' && gameboard[2][0] == 'O') {
+            System.out.println("Player 2 Wins");
+            return true;
+        }
+        return false;
+    }
+
 }
