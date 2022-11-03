dc87f19cfc14b3eb359f88ead14b6792d0368c81
MoeDev <moeFCIT20@gmail.com>
2022-11-03 21:40:11 +0300

    adding new id generator.

--- src/Player.java
+++ src/Player.java
@@ -12,14 +12,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.UUID;
 
 public class Player {
     private Socket s;
     private PrintWriter WriteToServer;
     private Scanner ReadFromServer;
-    
-   
+
     public static void main(String[] args) throws IOException, ParseException, SQLException {
 
         Player p = new Player();
@@ -29,81 +27,74 @@ public class Player {
 
     public void menu() throws IOException, ParseException, SQLException {
 
-        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cpit305-project", "root", "moe123");
+        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cpit305-project", "root",
+                "moe123");
         Statement stat = conn.createStatement();
+        PreparedStatement ps = conn.prepareStatement("");
 
         Scanner in = new Scanner(System.in);
         String choice = "0";
         String name = "";
         String pass = "";
-        String IDs = "";
+        int IDs = 0;
         while (true) {
             if (choice.equals("0")) {
-                do{
-                System.out.println("======================================================");    
-                System.out.println("\n==================== RPS Game ====================");
-                System.out.println("Welcome to our game\n");
-                System.out.println("1. New player?");
-                System.out.println("2. Login");
-                System.out.println("3. Exit");
-                System.out.print("Choose from above: ");
-                choice = in.next();
-                
-                if (choice.equals("1")) {
+                do {
+                    System.out.println("======================================================");
+                    System.out.println("\n==================== RPS Game ====================");
+                    System.out.println("Welcome to our game\n");
+                    System.out.println("1. New player?");
+                    System.out.println("2. Login");
+                    System.out.println("3. Exit");
+                    System.out.print("Choose from above: ");
+                    choice = in.next();
+
+                    if (choice.equals("1")) {
 
                         boolean usedName = false;
 
-                    
-                    do{
-                    System.out.println("Enter your username: ");
-                    name = in.next();
-                
-                    System.out.println("enter your password: ");
-                    pass = in.next();
-       
-                      usedName = check(name,stat,conn);
-                      
-                      if(usedName){
-                       System.out.println("username is used please try another one..");
-                        }
-                    
-                    
-                    }while(usedName);
-                    
-                    System.out.println("Hey " + name + " your registration has been successfully completed");
-                    IDs = UUID.randomUUID().toString().substring(2,5);
-                   System.out.println("Your ID is " + IDs);
-                     PreparedStatement ps = conn.prepareStatement("insert into player (ID, username, password, Wins) values(?,?,?,?)");
-                   
-                      ps.setString(1,IDs);
-                      ps.setString(2,name);
-                      ps.setString(3,pass);
-                      ps.setInt(4,0);
-                      
-                      ps.executeUpdate();
-                   
-                       
-                       
-                           
-                            
-                  
-                }
+                        do {
+                            System.out.println("Enter your username: ");
+                            name = in.next();
+
+                            System.out.println("enter your password: ");
+                            pass = in.next();
+
+                            usedName = checkReg(name, ps, conn);
+
+                            if (usedName) {
+                                System.out.println("username is used please try another one..");
+                            }
+
+                        } while (usedName);
+
+                        System.out.println("Hey " + name + " your registration has been successfully completed");
+                        IDs = generateID(conn);
+                        System.out.println("Your ID is " + IDs);
+                         ps = conn.prepareStatement("insert into player (ID, username, password, Wins) values(?,?,?,?)");
+
+                        ps.setInt(1, IDs);
+                        ps.setString(2, name);
+                        ps.setString(3, pass);
+                        ps.setInt(4, 0);
+
+                        ps.executeUpdate();
+
+                    }
 
                     else if (choice.equals("2")) {
                         boolean NotRegistered = false;
                         String TryOrBack = "";
 
-                    do{
-                    System.out.println("Enter your username: ");
-                    name = in.next();
-                    System.out.println("enter your password: ");
-                    pass = in.next();
-                                
+                        do {
+                            System.out.println("Enter your username: ");
+                            name = in.next();
+                            System.out.println("enter your password: ");
+                            pass = in.next();
 
-                
-                   NotRegistered = CheckLogin(name, pass, stat, conn);
-                            if(NotRegistered){
-                             System.out.println("Your username/password is incorrect ");
+                            NotRegistered = CheckLogin(name, pass, ps, conn);
+                            if (NotRegistered) {
+                                System.out.println("Your username/password is incorrect ");
                                 System.out.println("*type \"back\" to go back or \"press any key\" to continue trying..");
                                 TryOrBack = in.next();
 
@@ -118,18 +109,18 @@ public class Player {
 
                         } while (NotRegistered);
 
-                        if(!choice.equals("0")){
-                            PreparedStatement ps = conn.prepareStatement("select ID from player where username = ? and password = ?");
-                            
-                               ps.setString(1,name);
-                               ps.setString(2,pass);
-                               ps.execute();
-                              ResultSet r = ps.getResultSet();
-                              
-                            if(r.next()){
-                            IDs = r.getString("ID");
+                        if (!choice.equals("0")) {
+                             ps = conn.prepareStatement("select ID from player where username = ? and password = ?");
+
+                            ps.setString(1, name);
+                            ps.setString(2, pass);
+                            ps.execute();
+                            ResultSet r = ps.getResultSet();
+
+                            if (r.next()) {
+                                IDs = r.getInt("ID");
                             }
-                                }
+                        }
 
                     }
 
@@ -186,53 +177,49 @@ public class Player {
 
                     }
 
-            
-                    else if (choice.equals("2") ) {
+                    else if (choice.equals("2")) {
 
-                     
-                   
-                            do {
-                        
-                                ResultSet r = stat.executeQuery("select Wins from player where username = '"+name+"'");
-                                     if(r.next())
-                                     {
-                                        int winsNum = r.getInt("Wins");
-                                        
-                                        if(winsNum % 2 == 0){
-                                        System.out.println("You have won "+winsNum +" games\n" + "Press " + "1"+" to exit record page");
-                                        }
-                                        else{
-                                        System.out.println("You have won "+winsNum +" game\n" + "Press " + "1"+" to exit record page");
-                                        }
-                                     }                        
-                                          
-                                          choice = in.next();
-                        } while (!choice.equalsIgnoreCase("E") );
-    
+                        do {
+
+                            ResultSet r = stat.executeQuery("select Wins from player where username = '" + name + "'");
+                            if (r.next()) {
+                                int winsNum = r.getInt("Wins");
+
+                                if (winsNum % 2 == 0) {
+                                    System.out.println("You have won " + winsNum + " games\n" + "Press " + "1"
+                                            + " to exit record page");
+                                } else {
+                                    System.out.println("You have won " + winsNum + " game\n" + "Press " + "1"
+                                            + " to exit record page");
+                                }
+                            }
+
+                            choice = in.next();
+                        } while (!choice.equalsIgnoreCase("E"));
+
                     }
-    
-                    else if (choice.equals("3") ) {
-    
+
+                    else if (choice.equals("3")) {
+
                         do {
                             System.out.println("\n===== Rock Paper Scissors =====\n"
                                     + "* In RPS game you can play online with any player.\n" +
                                     "* You will play for THREE rounds.\n" +
                                     "* The one gets the highest points will win the game.\n\n" +
-    
+
                                     "* Press " + "1" + " to choose Rock\n" +
                                     "* Press " + "2" + " to choose Paper\n" +
                                     "* Press " + "3" + " tp choose Scissor\n" +
-                                    "Press "+"E"+" to exit tutorial page");
+                                    "Press " + "E" + " to exit tutorial page");
                             choice = in.next();
                         } while (!choice.equalsIgnoreCase("E"));
-    
+
                     }
 
-
-                else if(choice.equals("4") ){
-                    System.out.println("Thank you for trying our game ^_^");
-                    break;
-                }
+                    else if (choice.equals("4")) {
+                        System.out.println("Thank you for trying our game ^_^");
+                        break;
+                    }
 
                 } while (!choice.equals("1") && !choice.equals("2") && !choice.equals("3") && !choice.equals("4"));
 
@@ -245,27 +232,25 @@ public class Player {
 
     }
 
-    private boolean check(String name, Statement stat, Connection conn) throws SQLException{
-    
-        PreparedStatement ps = conn.prepareStatement("select * from player where username = ?");
-           ps.setString(1, name);
-                   ps.execute();
-                   ResultSet r = ps.getResultSet();
-                  if(r.next()){
-                     return true;
-               }
-                  else{
-                      return false;
-                  }
+    private boolean checkReg(String name, PreparedStatement ps, Connection conn) throws SQLException {
 
+         ps = conn.prepareStatement("select * from player where username = ?");
+        ps.setString(1, name);
+        ps.execute();
+        ResultSet r = ps.getResultSet();
+        if (r.next()) {
+            return true;
+        } else {
+            return false;
+        }
 
-}
+    }
 
     private void ConnectToServer() throws IOException {
         s = new Socket("localhost", 5000);
 
-        InputStream  in=  s.getInputStream();
-        OutputStream out =  s.getOutputStream();
+        InputStream in = s.getInputStream();
+        OutputStream out = s.getOutputStream();
 
         WriteToServer = new PrintWriter(out, true);
         ReadFromServer = new Scanner(in);
@@ -279,20 +264,34 @@ public class Player {
 
     }
 
-    private boolean CheckLogin(String name, String pass, Statement stat, Connection conn) throws SQLException {
-        PreparedStatement ps = conn.prepareStatement("select ID from player where username = ? and password = ?");
-                
-                   ps.setString(1,name);
-                   ps.setString(2,pass);
-                   ps.execute();
-                  ResultSet r = ps.getResultSet();
-                    if (r.next()) {
-                        return false;
-                    }
+    private boolean CheckLogin(String name, String pass, PreparedStatement ps, Connection conn) throws SQLException {
+         ps = conn.prepareStatement("select ID from player where username = ? and password = ?");
 
-                    else {
-                        return true;
-                    }
-}
+        ps.setString(1, name);
+        ps.setString(2, pass);
+        ps.execute();
+        ResultSet r = ps.getResultSet();
+        if (r.next()) {
+            return false;
+        }
 
+        else {
+            return true;
+        }
+    }
+
+    private static int generateID(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+
+        ResultSet rs = stmt.executeQuery("SELECT MAX(id) as 'new_id' FROM player;");
+
+        if (rs.next() == false) {
+            return 1;
+        }
+
+        else {
+            return rs.getInt("new_id") + 1;
+
+        }
+    }
 }
