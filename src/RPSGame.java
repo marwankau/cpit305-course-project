a2fe593c46e21b6f967883232d723ec12f59e48f
MoeDev <moeFCIT20@gmail.com>
2022-11-03 13:19:47 +0300

    edit code format

--- src/RPSGame.java
+++ src/RPSGame.java
@@ -19,68 +19,55 @@ public class RPSGame {
     static Socket s2 = new Socket();
     static Socket s = new Socket();
 
-public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException {
 
-    
-    Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cpit305-project", "root", "moe123");
-    Statement stat = conn.createStatement();
-   
+        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/cpit305-project", "root",
+                "moe123");
+        Statement stat = conn.createStatement();
 
-    
-    
-    ServerSocket server = new ServerSocket(5000);
-    System.out.println("server connected..");
-    try  {
-        while(!server.isClosed()){
+        ServerSocket server = new ServerSocket(5000);
+        System.out.println("server connected..");
+        try {
+            while (!server.isClosed()) {
 
-            if(Players <= 1){
-          s = server.accept();
-          Players++;
-          OutputStream out =  s.getOutputStream();
- 
-         
-         WriteToPlayer1 = new PrintWriter(out,true);
-         WriteToPlayer1.println("player #"+1+" has connected"); 
-         WriteToPlayer1.println("Wait for Player2..");
-         
-          s2 = server.accept();
-         
-          Players++;
-          OutputStream out2 =  s2.getOutputStream();
- 
-         WriteToPlayer2 = new PrintWriter(out2,true);
-         WriteToPlayer2.println("player #"+1+" has connected"); 
-         WriteToPlayer2.println("player #"+2+" has connected"); 
-  
-         System.out.println("2 are connected lets start game");
- 
-          ServerSideConnection ssc = new ServerSideConnection(s, s2, Players, stat);
-             ssc.start();
-             }
+                if (Players <= 1) {
+                    s = server.accept();
+                    Players++;
+                    OutputStream out = s.getOutputStream();
 
+                    WriteToPlayer1 = new PrintWriter(out, true);
+                    WriteToPlayer1.println("player #" + 1 + " has connected");
+                    WriteToPlayer1.println("Wait for Player2..");
 
-            else{
-                 Players = ssc.getPlayerCount();
-                 if(Players == 0){
-                     s.close();
-                     s2.close();
-                 }
-                 
- 
-             }
+                    s2 = server.accept();
+
+                    Players++;
+                    OutputStream out2 = s2.getOutputStream();
+
+                    WriteToPlayer2 = new PrintWriter(out2, true);
+                    WriteToPlayer2.println("player #" + 1 + " has connected");
+                    WriteToPlayer2.println("player #" + 2 + " has connected");
+
+                    System.out.println("2 are connected lets start game");
+
+                    ServerSideConnection ssc = new ServerSideConnection(s, s2, Players, stat);
+                    ssc.start();
+                }
+
+                else {
+                    Players = ssc.getPlayerCount();
+                    if (Players == 0) {
+                        s.close();
+                        s2.close();
+                    }
+
+                }
             }
-        }
-     catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
-        
+
+        }
+
     }
 
-
-
-}
-
-
-
- 
-     
 }
