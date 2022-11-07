aad0a948e1f101f6708a8679f520c3a96d71b814
RakanSalama <superrakan@hotmail.com>
2022-11-07 19:34:25 +0300

    Client menu functions

--- src/Client.java
+++ src/Client.java
@@ -13,8 +13,10 @@ public class Client {
         String ReceptionPassword[] = { "RakanS", "MoathS", "AbdulrahmanN" };
         String ReceptionName[] = { "Rakan", "Moath", "Abdulrahman" };
 
-        Boolean check = true; // For the while loop, If it change the loop will stop ( it will only change if the user did login)
-        while (check) { // this loop is made because if the receptionist didn't enter a correct username or password, it will ask him again and again.
+        Boolean check = true; // For the while loop, If it change the loop will stop ( it will only change if
+                              // the user did login)
+        while (check) { // this loop is made because if the receptionist didn't enter a correct username
+                        // or password, it will ask him again and again.
 
             // Here we take Username And password
             Scanner scanner = new Scanner(System.in);
@@ -40,23 +42,49 @@ public class Client {
 
     public static void connect(String ReceptionName[], int i, Scanner scanner) throws IOException {
         String line;
-        int choice;
-        
+        String choice;
+
         try {
             Socket client = new Socket("localhost", 2000); // it will connect the receptionist with the server
             // -----------------------------------------------------------------------------------------------------------
             InputStream in = client.getInputStream(); // Read bytes from server
-            OutputStream out = client.getOutputStream(); // Send bytes to server // Read and write between server and receptionist
+            OutputStream out = client.getOutputStream(); // Send bytes to server // Read and write between server and
+                                                         // receptionist
+            Scanner sc = new Scanner(System.in);
             Scanner receiver = new Scanner(in); // Read normally
             PrintWriter writer = new PrintWriter(out, true); // write normally
-            Menu m = new Menu(writer);
             // -----------------------------------------------------------------------------------------------------------
-            writer.println("( " + ReceptionName[i] + " ) " + "is connected\n"); // This will send a notfication to the server for whom did join.
-            m.MainMenu();
-            choice = scanner.nextInt();
-            writer.println(choice);
-
-
+            writer.println("( " + ReceptionName[i] + " ) " + "is connected\n"); // This will send a notfication to the
+                                                                                // server for whom did join.
+            while (true) {
+                System.out.println("=====================================");
+                System.out.println("||Welcome to hotel database browser||");
+                System.out.println("=====================================");
+                System.out.println("1. List all rooms");
+                System.out.println("2. Update room");
+                System.out.println("3. Search by Room Number");
+                System.out.println("Note: type exit: to close the system");
+                System.out.println("=====================================");
+                System.out.print("choose: ");
+                choice = sc.nextLine();
+                if (choice.equals("1")) {
+                    System.out.println("HEEY");
+                }
+                else if (choice.equals("2")) {
+                    
+                    System.out.println("AAAAAAAAAAAA");
+                }
+                else if (choice.equals("3")) {
+                    System.out.println("BBBBBBBBBBBB");
+                }
+                else if (choice.equalsIgnoreCase("exit:")) {
+                    System.out.println("Thank you for using our system ");
+                    break;
+                }
+                else{
+                    System.out.println("Wrong input");
+                }
+            }
         } catch (ConnectException e) { // if the server was offline it will handel it
             System.out.println("Server is offline");
         }
