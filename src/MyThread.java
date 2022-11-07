6ca898c5258a310bbc1cf75422f36279f3d1934e
RakanSalama <superrakan@hotmail.com>
2022-11-07 19:34:31 +0300

    Client menu functions

--- src/MyThread.java
+++ src/MyThread.java
@@ -3,10 +3,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.SocketException;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
+
+import javax.sound.midi.Receiver;
 
 public class MyThread extends Thread {
     Socket client;
@@ -19,23 +18,14 @@ public class MyThread extends Thread {
     public void run() {
         try {
             String line;
-            int choice;
             InputStream in = client.getInputStream();
             OutputStream out = client.getOutputStream();
             Scanner receiver = new Scanner(in);
             PrintWriter writer = new PrintWriter(out, true);
             // -----------------------------------------------------------------------------
-            while (true) {
-                line = receiver.nextLine(); // get the msg from the client
-                System.out.println(line); // ... is connected //give an information about who is connected to the server
-                try {
-                    choice = receiver.nextInt();
-                    System.out.println("The choice was: " + choice);
-                } catch (InputMismatchException e) {
-                    System.out.println("Wrong input");
-                }
-                writer.close();
-            }
+            line = receiver.nextLine(); // get the msg from the client
+            System.out.println(line); // ... is connected //give an information about who is connected to the server
+            
         } catch (IOException e) {
         }
     }
--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,18 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+
 
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(2000);
 
         System.out.println("Server is Online");
-        while(true){
             while (true) {
                 Socket client = server.accept();
                 
                 MyThread thread = new MyThread(client);
                 thread.start();
-               
-    
-            }
         }
     }
 }
