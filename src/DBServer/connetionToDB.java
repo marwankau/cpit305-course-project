fe20a6da15424347e03a32a1228c2fa0083a6d76
MAlhazmi-digitect <m.alhazmi@digitect.com>
2022-11-09 19:20:17 +0300

    db path

--- src/DBServer/connetionToDB.java
+++ src/DBServer/connetionToDB.java
@@ -1,4 +1,5 @@
 package DBServer;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,34 +11,30 @@ import java.sql.Statement;
 
 import org.sqlite.SQLiteConnection;
 
-
-public class connetionToDB { 
-    public static void main(String[] args)  {
+public class connetionToDB {
+    public static void main(String[] args) {
         Connection conn;
         System.out.println("Server waiting for connection...");
-        try  {
+        try {
 
-          conn  = (Connection) DriverManager.getConnection("jdbc:sqlite:C:/Users/kheda/Desktop/cpit305-course-project/mydata");
+            conn = (Connection) DriverManager.getConnection("jdbc:sqlite:cpit305-course-project/mydata");
             ServerSocket server = new ServerSocket(2000);
-        
-           
-while(true){
 
-    Socket socket = server.accept();
-     
-   //  new Sender(socket,conn).start();
-     new Receiver(socket,conn).start();
-    
-     
-       
-    } 
-     
-} catch (SQLException e) {
-    // TODO Auto-generated catch block
-    System.out.println(e);
-}catch( IOException e){
-    System.out.println(e);
-}
+            while (true) {
 
-}
+                Socket socket = server.accept();
+
+                // new Sender(socket,conn).start();
+                new Receiver(socket, conn).start();
+
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
 }
