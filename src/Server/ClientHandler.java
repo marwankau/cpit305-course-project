5c8fbdd5077255448af63ed293f0ff67559fb138
MohammedAlnajrani <m.alnajrani1999@gmail.com>
2022-11-14 23:07:30 +0300

    create patient query, menu and update doctor query

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -68,7 +69,14 @@ public class ClientHandler extends Thread{
           deleteDoctor(dos, id);
         }
       }else if(Integer.parseInt(line) == 2){
-        dos.writeUTF("add a doctor");
+        dos.writeUTF("---------Manage Patients---------");
+        dos.writeUTF("1. Display all patients");
+        dos.writeUTF("2. Add patients");
+        dos.writeUTF("3. Delete patients");
+        dos.writeUTF("4. Update patients");
+        dos.writeUTF("Enter your choice: ");
+        line = dis.readUTF();
+        int choice = Integer.parseInt(line);
       }
     }
        
@@ -103,7 +111,7 @@ public class ClientHandler extends Thread{
       // TODO: handle exception
     }
   }
-
+//insert doctor
   public void insertDoctor(DataOutputStream dos, String doctor_name, String doctor_speciality) throws IOException{
     try {
       PreparedStatement ps = conn.prepareStatement("INSERT INTO Doctor (doctor_name, doctor_speciality) VALUES (?, ?);");
@@ -117,7 +125,7 @@ public class ClientHandler extends Thread{
       dos.writeUTF("Inserting failed");
     }
   }
-
+//delete doctor
   public void deleteDoctor(DataOutputStream dos, int doctor_id) throws IOException{
     try {
       PreparedStatement ps = conn.prepareStatement("DELETE FROM Doctor WHERE doctor_id = ?");
@@ -128,6 +136,44 @@ public class ClientHandler extends Thread{
 
     } catch (Exception e) {
       dos.writeUTF("deleting failed");
+    }
+  }
+  //insert patient
+  public void insertPatient(DataOutputStream dos, String patient_name, Date patient_dob) throws IOException{
+    try {
+      PreparedStatement ps = conn.prepareStatement("INSERT INTO Patient (patient_name, patient_DOB) VALUES (?, ?);");
+      ps.setString(1, patient_name);
+      ps.setDate(2, patient_dob);
+      boolean rs = ps.execute();
+       dos.writeUTF("Patient has been successfully inserted");
+    } catch (Exception e) {
+      dos.writeUTF("Inserting has been failed");
+    }
+  }
+  //delelet Patients
+  public void deletePatient(DataOutputStream dos, int Patient_id) throws IOException{
+    try {
+      PreparedStatement ps = conn.prepareStatement("DELETE FROM Patient WHERE Patient_id = ?");
+      ps.setInt(1, Patient_id);
+      boolean rs = ps.execute();
+       dos.writeUTF("Patient has been successfully deleted");
+      
+
+    } catch (Exception e) {
+      dos.writeUTF("delete has been failed");
+    }
+  }
+  public void updateDoctor(DataOutputStream dos, String doctor_speciality ,int doctor_id) throws IOException{
+    try {
+      PreparedStatement ps = conn.prepareStatement("UPDATE FROM Doctor SET doctor_speciality= ?  WHERE doctor_id = ?");
+      ps.setString(1, doctor_speciality);
+      ps.setInt(2, doctor_id);
+      boolean rs = ps.execute();
+       dos.writeUTF("Doctor has been successfully updated");
+      
+
+    } catch (Exception e) {
+      dos.writeUTF("update has been failed");
     }
   }
 
