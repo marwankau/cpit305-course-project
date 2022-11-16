2e23e435da39a9abc89fc3a56dab89bfad2b980c
1939123 <114641673+1939123@users.noreply.github.com>
2022-11-16 18:21:42 +0300

    Add files via upload

---
+++ src/BankServer.java
@@ -0,0 +1,41 @@
+package cpit_305_finalproject;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BankServer { 
+    
+
+    private static ArrayList<ClientHandler> clients; 
+    private static ExecutorService pool = Executors.newFixedThreadPool(10);
+    public static int k=0;
+   
+
+     
+    public static void main(String[] args) throws IOException {
+        
+        
+        ServerSocket ser = new ServerSocket(50999);
+        clients = new ArrayList<>(); 
+        
+        while (true) {
+            k++;
+           
+            System.out.printf("Waiting for clients on port %d...\n", ser.getLocalPort());
+            Socket c = ser.accept();
+            System.out.println("\nClient "+k+" Connected...... "); 
+           
+            ClientHandler ClThread = new ClientHandler(c,k);
+            clients.add(ClThread);
+            pool.execute(ClThread);
+           
+        }
+
+    }
+  
+}
+
+
